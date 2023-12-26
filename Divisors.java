e07fddfe826af83d6406cc229fa0dfad884eaa67
zionabebe34 <zion.abebe@post.runi.ac.il>
2023-12-26 18:56:00 +0200

    add the first exercise

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,19 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		
+		int x = Integer.parseInt(args[0]); 
+		int count = 1;
+
+
+		while ( count <= x) {
+			if ( (x % count) == 0) {
+				System.out.println( count ); 
+				
+			}
+			count ++; 
+		}
+
+
 	}
 }
