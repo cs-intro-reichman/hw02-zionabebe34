e49ab480111c9ce7a95095216bdad129e6205afb
zionabebe34 <zion.abebe@post.runi.ac.il>
2023-12-27 14:18:41 +0200

    add the InOrder file

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,22 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		String str = " "; 
+		int num = (int)( (Math.random() * 10) + 1 );
+		str = "" + num;  
+		
+
+		while (true) {
+			int num1 = (int) ( (Math.random() * 10) + 1 ); 
+			if ( num1 > num ) {
+				String temp = "" + num1; //storage in temp variable for adding later to str
+				str = str + " " + temp; 
+				num = num1;
+			} else {
+				break; 
+			}
+		}
+		System.out.println(str); 
+
 	}
 }
