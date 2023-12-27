a5998112eeb1bba5a3d38b0ca5431702afbd2c8d
zionabebe34 <zion.abebe@post.runi.ac.il>
2023-12-27 15:12:40 +0200

    i add the perfect exercise

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,35 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]); 
+		String divisors = " "; 
+		int div = 0; 
+		String newDiv = " "; 
+
+		for  (int i = 1; i < num; i++) {
+			if ( (num % i) == 0) {
+				div = div + i; 
+				String temp = "" + i; 
+				divisors += temp + "" + "+"; 
+			}
+		}
+
+		for (int k = 0; k < divisors.length() - 1; k++ ) {
+			newDiv = newDiv + divisors.charAt(k); 
+
+		}
+
+
+
+
+
+		if (div == num) {
+			System.out.println( num + " " + "is a perfect number since" + " " + num + " " +"="+ "" + newDiv); 
+		} else {
+			System.out.println( num + " " + "is not a perfect number");
+		}
+
+	
+
 	}
 }
