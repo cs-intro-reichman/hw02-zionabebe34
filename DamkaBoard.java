3aaab1eee5187dd632293b2fc83db3deb8ecb1db
zionabebe34 <zion.abebe@post.runi.ac.il>
2023-12-28 11:00:50 +0200

    fixing damka board

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,26 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]); 
+		int count = 0;
+		int counter = 0;
+
+
+		for (int i = 1; i <= (num * num ); i++) {
+			System.out.print(" *"); //print the pattern 
+			if ( i % num == 0) {
+				System.out.println(); //down the line 
+
+				if (count == counter){ // after we down the line its check if the line need to move right
+					System.out.print(" ");
+					counter = counter + 2;  // the counts help us to sure the progam will move right every two lines 
+				}
+				 
+			count++;
+
+			}
+			 
+		}
+
 	}
 }
