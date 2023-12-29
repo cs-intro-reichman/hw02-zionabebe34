2bc269af2835b47451be29f36e613aa780bb6737
zionabebe34 <zion.abebe@post.runi.ac.il>
2023-12-29 23:36:39 +0200

    another change for the grading

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -14,7 +14,7 @@ public class DamkaBoard {
 				System.out.println(); //down the line 
 
 				if (count == counter){ // after we down the line its check if the line need to move right
-					System.out.print(" ");
+					System.out.print("  ");
 					counter = counter + 2;  // the counts help us to sure the progam will move right every two lines 
 				}
 				 
